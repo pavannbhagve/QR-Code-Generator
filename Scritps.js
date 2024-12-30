636a1d75b84ecd139b4c99c7496c4a8f55acf546
PB <pb@gmail.com>
2024-12-30 14:55:40 +0530

    css add

---
+++ Scritps.js
@@ -0,0 +1,7 @@
+const qrinput = document.querySelector("qr-input");
+const qrbut = document.querySelector("qr-button");
+const qrimg = document.querySelector("qr-img");
+
+console.log(qrbut,qrbut,qrimg);
+
+
