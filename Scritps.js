ca72526f6059aa1fb2238f24eaa570022515dc5f
PB <pb@gmail.com>
2024-12-30 15:31:30 +0530

    add js

--- Scritps.js
+++ Scritps.js
@@ -1,7 +1,25 @@
-const qrinput = document.querySelector("qr-input");
-const qrbut = document.querySelector("qr-button");
-const qrimg = document.querySelector("qr-img");
+const qrinput = document.querySelector("#qr-input");
+const qrbut = document.querySelector(".qr-button");
+const qrimg = document.querySelector("#qr-img");
 
-console.log(qrbut,qrbut,qrimg);
+qrbut.addEventListener("click", ()=>{
+
+    const inputValue = qrinput.value;
+
+    if ( ! inputValue ) {
+
+        alert("Please enter a valid URL");
+        return;
+        
+    }else{
+
+        qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+        qrimg.alt =`QR Code for ${inputValue}`;
+    }
+    
+
+})
+
+
 
 
